148a53a570e59ecd9d8f0061c785c0564f2661bb
Jose Arteaga <jarteaga49894@gmail.com>
2023-04-26 13:55:27 -0700

    db files

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db; 
+
+USE staff_db; 
+
+CREATE TABLE department ( 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role ((
+    if INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INT, 
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL 
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT, 
+    manager_id INT, 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERCNES employee(id)
+    ON DELETE SET NULL
+);
